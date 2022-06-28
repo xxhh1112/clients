f8f4409501e5d0cb5294d7a2193b366c5f068069
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-06-28 13:12:10 +0200

    feat(common): expand vault filter with updateability

--- libs/angular/src/modules/vault-filter/models/new-vault-filter.model.spec.ts
+++ libs/angular/src/modules/vault-filter/models/new-vault-filter.model.spec.ts
@@ -2,12 +2,28 @@ import { VaultFilter } from "./new-vault-filter.model";
 
 describe("VaultFilter", () => {
   describe("update", () => {
-    it("should be equal when not updating any row", () => {
+    it("should be equal when not updating any fields", () => {
       const filter = new VaultFilter();
 
       const result = filter.update({});
 
       expect(result).toEqual(filter);
+    });
+
+    it("should not be equal when updating fields", () => {
+      const filter = new VaultFilter({ selectedFolder: true });
+
+      const result = filter.update({ selectedFolder: false });
+
+      expect(result).not.toEqual(filter);
+    });
+
+    it("should return filter with new field value when updating field", () => {
+      const filter = new VaultFilter({ selectedFolder: false });
+
+      const result = filter.update({ selectedFolder: true });
+
+      expect(result.selectedFolder).toBe(true);
     });
   });
 });
--- libs/angular/src/modules/vault-filter/models/new-vault-filter.model.ts
+++ libs/angular/src/modules/vault-filter/models/new-vault-filter.model.ts
@@ -1,5 +1,27 @@
-export class VaultFilter {
-  update(options: Record<string, unknown>) {
-    return this;
+const DefaultOptions: VaultFilterOptions = {
+  selectedFolder: false,
+};
+
+export type VaultFilterOptions = Readonly<{
+  selectedFolder: boolean;
+}>;
+
+export class VaultFilter implements VaultFilterOptions {
+  constructor(private readonly options: Partial<VaultFilterOptions> = {}) {
+    this.options = {
+      ...DefaultOptions,
+      ...options,
+    };
+  }
+
+  get selectedFolder() {
+    return this.options.selectedFolder;
+  }
+
+  update(options: Partial<VaultFilterOptions>) {
+    return new VaultFilter({
+      ...this.options,
+      ...options,
+    });
   }
 }
