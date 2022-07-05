eee2cee03399e6adfba2b19d92bdc1905a361d5b
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-07-05 08:54:45 +0200

    chore: cleanup

--- libs/components/src/button-group/button-group.component.ts
+++ libs/components/src/button-group/button-group.component.ts
@@ -1,18 +1,11 @@
-import { HostBinding, Component, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 let nextId = 0;
 
 @Component({
   selector: "bit-button-group",
   templateUrl: "./button-group.component.html",
-  host: {
-    class: "",
-  },
 })
 export class ButtonGroupComponent {
   @Input() name = `bit-button-group-${nextId++}`;
-
-  @HostBinding("class") get classList() {
-    return [];
-  }
 }
