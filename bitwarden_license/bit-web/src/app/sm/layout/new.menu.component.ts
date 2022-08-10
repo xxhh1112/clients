6da1af741a0ed833abc2ec0a2b9b7bb786aab7fa
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2022-08-10 16:16:10 -0500

    [SM-63] Secrets List overview (#3239)
    The purpose of this PR is to create a new component for the Secrets Manager project where all the secrets associated to a specific organization ID can be viewed.

---
+++ bitwarden_license/bit-web/src/app/sm/layout/new.menu.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "sm-new-menu",
+  templateUrl: "./new.menu.component.html",
+})
+export class NewMenuComponent {}
--- bitwarden_license/bit-web/src/app/sm/secrets/secrets.module.ts
+++ bitwarden_license/bit-web/src/app/sm/secrets/secrets.module.ts
@@ -1,12 +1,24 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { SharedModule } from "src/app/shared";
+
+import { FilterComponent } from "../layout/filter.component";
+import { HeaderComponent } from "../layout/header.component";
+import { NewMenuComponent } from "../layout/new.menu.component";
+
+import { SecretsListComponent } from "./secrets-list.component";
 import { SecretsRoutingModule } from "./secrets-routing.module";
 import { SecretsComponent } from "./secrets.component";
 
 @NgModule({
-  imports: [CommonModule, SecretsRoutingModule],
-  declarations: [SecretsComponent],
+  imports: [SharedModule, SecretsRoutingModule],
+  declarations: [
+    SecretsComponent,
+    SecretsListComponent,
+    HeaderComponent,
+    FilterComponent,
+    NewMenuComponent,
+  ],
   providers: [],
 })
 export class SecretsModule {}
