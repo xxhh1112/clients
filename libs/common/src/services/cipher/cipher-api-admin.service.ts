1d947af271201a5f20817789a1e9e1c03cc021c1
dynwee <onwudiweokeke@gmail.com>
2022-08-13 00:49:06 +0100

    Creating the abstraction and the services for cipher

---
+++ libs/common/src/services/cipher/cipher-api-admin.service.ts
@@ -0,0 +1,57 @@
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { CipherApiAdminServiceAbstraction } from "@bitwarden/common/abstractions/cipher/cipher-api-admin.service.abstraction";
+import { CipherBulkDeleteRequest } from "@bitwarden/common/models/request/cipherBulkDeleteRequest";
+import { CipherCollectionsRequest } from "@bitwarden/common/models/request/cipherCollectionsRequest";
+import { CipherCreateRequest } from "@bitwarden/common/models/request/cipherCreateRequest";
+import { CipherRequest } from "@bitwarden/common/models/request/cipherRequest";
+import { CipherResponse } from "@bitwarden/common/models/response/cipherResponse";
+
+export class CipherApiAdminService implements CipherApiAdminServiceAbstraction {
+  constructor(private apiService: ApiService) {}
+
+  async getCipherAdmin(id: string): Promise<CipherResponse> {
+    const r = await this.apiService.send("GET", "/ciphers/" + id + "/admin", null, true, true);
+    return new CipherResponse(r);
+  }
+
+  async postCipherAdmin(request: CipherCreateRequest): Promise<CipherResponse> {
+    const r = await this.apiService.send("POST", "/ciphers/admin", request, true, true);
+    return new CipherResponse(r);
+  }
+  async putCipherAdmin(id: string, request: CipherRequest): Promise<CipherResponse> {
+    const r = await this.apiService.send("PUT", "/ciphers/" + id + "/admin", request, true, true);
+    return new CipherResponse(r);
+  }
+  deleteCipherAdmin(id: string): Promise<any> {
+    return this.apiService.send("DELETE", "/ciphers/" + id + "/admin", null, true, false);
+  }
+  deleteManyCiphersAdmin(request: CipherBulkDeleteRequest): Promise<any> {
+    return this.apiService.send("DELETE", "/ciphers/admin", request, true, false);
+  }
+  putDeleteCipherAdmin(id: string): Promise<any> {
+    return this.apiService.send("PUT", "/ciphers/" + id + "/delete-admin", null, true, false);
+  }
+  putDeleteManyCiphersAdmin(request: CipherBulkDeleteRequest): Promise<any> {
+    return this.apiService.send("PUT", "/ciphers/delete-admin", request, true, false);
+  }
+  async putRestoreCipherAdmin(id: string): Promise<CipherResponse> {
+    const r = await this.apiService.send(
+      "PUT",
+      "/ciphers/" + id + "/restore-admin",
+      null,
+      true,
+      true
+    );
+    return new CipherResponse(r);
+  }
+
+  putCipherCollectionsAdmin(id: string, request: CipherCollectionsRequest): Promise<any> {
+    return this.apiService.send(
+      "PUT",
+      "/ciphers/" + id + "/collections-admin",
+      request,
+      true,
+      false
+    );
+  }
+}
