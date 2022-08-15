4abe802827487a55665c848574c0f8bc4872ab89
gbubemismith <gsmithwalter@gmail.com>
2022-08-15 10:27:19 -0600

    renamed model property

--- libs/common/src/models/request/passwordlessCreateAuthRequest.ts
+++ libs/common/src/models/request/passwordlessCreateAuthRequest.ts
@@ -3,7 +3,7 @@ import { AuthRequestType } from "../../enums/authRequestType";
 export class PasswordlessCreateAuthRequest {
   constructor(
     readonly email: string,
-    readonly requestDeviceIdentifier: string,
+    readonly deviceIdentifier: string,
     readonly publicKey: string,
     readonly type: AuthRequestType,
     readonly accessCode: string,
