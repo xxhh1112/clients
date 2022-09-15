11551467c4cb9d3566be2ebe73fa470be3924e3c
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2022-09-15 09:58:40 -0500

    Fix SM lint errors (#3526)
    * Fix SM lint errors
    
    * Update bitwarden_license/bit-web/src/app/sm/secrets/secrets.component.ts
    
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>
    
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- bitwarden_license/bit-web/src/app/sm/secrets/secrets.component.ts
+++ bitwarden_license/bit-web/src/app/sm/secrets/secrets.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { switchMap, takeUntil } from "rxjs/operators";
 
 import { SecretResponse } from "./responses/secret.response";
 import { SecretApiService } from "./secret-api.service";
@@ -8,18 +10,29 @@ import { SecretApiService } from "./secret-api.service";
   selector: "sm-secrets",
   templateUrl: "./secrets.component.html",
 })
-export class SecretsComponent implements OnInit {
+export class SecretsComponent implements OnInit, OnDestroy {
   private organizationId: string;
+  private destroy$ = new Subject<void>();
 
   secrets: SecretResponse[];
 
   constructor(private route: ActivatedRoute, private secretsApiService: SecretApiService) {}
 
   ngOnInit() {
-    this.route.params.subscribe(async (params: any) => {
-      this.organizationId = params.organizationId;
-      await this.getSecrets();
-    });
+    this.route.params
+      .pipe(
+        switchMap(async (params) => {
+          this.organizationId = params.organizationId;
+          await this.getSecrets();
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   private async getSecrets() {
