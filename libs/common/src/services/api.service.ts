ea35e115fd389b4ae1e5014d7f2f9165884aecb0
Addison Beck <addisonbeck1@gmail.com>
2022-09-15 16:46:58 -0400

    [SG-656] Use a captcha bypass during registration (#3531)
    * Use a captcha bypass during registration
    
    The trial initiation flow has a registration step that automatically
    does a login in the background. This has Captcha problems, namely that
    it can spawn two captchas in a row - one during registration and one
    during login. This is not ideal UX, so we've added a bypass token that
    returns from the registration endpoint that can be used to skip the next
    captcha.
    
    * [review] Introduce ICaptcheProtectedResponse

--- libs/common/src/services/api.service.ts
+++ libs/common/src/services/api.service.ts
@@ -104,6 +104,7 @@ import { VerifyEmailRequest } from "../models/request/verifyEmailRequest";
 import { ApiKeyResponse } from "../models/response/apiKeyResponse";
 import { AttachmentResponse } from "../models/response/attachmentResponse";
 import { AttachmentUploadDataResponse } from "../models/response/attachmentUploadDataResponse";
+import { RegisterResponse } from "../models/response/authentication/registerResponse";
 import { BillingHistoryResponse } from "../models/response/billingHistoryResponse";
 import { BillingPaymentResponse } from "../models/response/billingPaymentResponse";
 import { BillingResponse } from "../models/response/billingResponse";
@@ -356,17 +357,18 @@ export class ApiService implements ApiServiceAbstraction {
     return this.send("POST", "/accounts/password-hint", request, false, false);
   }
 
-  postRegister(request: RegisterRequest): Promise<any> {
-    return this.send(
+  async postRegister(request: RegisterRequest): Promise<RegisterResponse> {
+    const r = await this.send(
       "POST",
       "/accounts/register",
       request,
       false,
-      false,
+      true,
       this.platformUtilsService.isDev()
         ? this.environmentService.getIdentityUrl()
         : this.environmentService.getApiUrl()
     );
+    return new RegisterResponse(r);
   }
 
   async postPremium(data: FormData): Promise<PaymentResponse> {
