9ddcf9c37221da72f5c0df9e77cdb59aed20030a
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2022-09-22 09:52:23 -0400

    Update bitwarden_license/bit-web/src/app/sm/shared/sm-shared.module.ts
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- bitwarden_license/bit-web/src/app/sm/shared/sm-shared.module.ts
+++ bitwarden_license/bit-web/src/app/sm/shared/sm-shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 
 import { SharedModule } from "src/app/shared";
 
-// Register the locales for the application
 import { FilterComponent } from "../layout/filter.component";
 import { HeaderComponent } from "../layout/header.component";
 import { NewMenuComponent } from "../layout/new-menu.component";
