6b3848da3329089513edc6f8290680d7de815477
Jacob Fink <jfink@bitwarden.com>
2022-09-26 15:30:51 -0400

    [EC-14] fix linter

--- apps/desktop/src/app/vault/vault-filter/vault-filter.module.ts
+++ apps/desktop/src/app/vault/vault-filter/vault-filter.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { DeprecatedVaultFilterService as DeprecatedVaultFilterServiceAbstraction } from "@bitwarden/angular/abstractions/deprecated-vault-filter.service";
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { VaultFilterService } from "@bitwarden/angular/vault/vault-filter/services/vault-filter.service";
-import { DeprecatedVaultFilterService as DeprecatedVaultFilterServiceAbstraction } from "@bitwarden/angular/abstractions/deprecated-vault-filter.service";
 
 import { CollectionFilterComponent } from "./filters/collection-filter.component";
 import { FolderFilterComponent } from "./filters/folder-filter.component";
