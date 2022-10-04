5adac02fd24a7c6309e1ff6a984e2faa7ce2ff2f
Jacob Fink <jfink@bitwarden.com>
2022-10-03 20:25:18 -0400

    [EC-14] Resolve merge conflicts

--- apps/web/src/app/organizations/vault/collection-badge/collection-badge.module.ts
+++ apps/web/src/app/organizations/vault/collection-badge/collection-badge.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from "@angular/core";
 
-import { VaultSharedModule } from "src/app/vault/shared/vault-shared.module";
-
 import { CollectionNameBadgeComponent } from "./collection-name.badge.component";
 
 @NgModule({
-  imports: [VaultSharedModule],
   declarations: [CollectionNameBadgeComponent],
   exports: [CollectionNameBadgeComponent],
 })
--- apps/web/src/app/organizations/vault/group-badge/group-badge.module.ts
+++ apps/web/src/app/organizations/vault/group-badge/group-badge.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from "@angular/core";
 
-import { VaultSharedModule } from "src/app/vault/shared/vault-shared.module";
-
 import { GroupNameBadgeComponent } from "./group-name-badge.component";
 
 @NgModule({
-  imports: [VaultSharedModule],
   declarations: [GroupNameBadgeComponent],
   exports: [GroupNameBadgeComponent],
 })
