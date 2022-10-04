8a3dd049ca250401fbdb8b2c11dd76cb2f27d1bb
Matt Gibson <mgibson@bitwarden.com>
2022-10-04 13:35:11 -0400

    Move AccountService.delete to Internal

--- libs/common/src/abstractions/account/account-api.service.abstraction.ts
+++
@@ -1,5 +0,0 @@
-import { SecretVerificationRequest } from "../../models/request/secretVerificationRequest";
-
-export abstract class AccountApiService {
-  abstract deleteAccount(request: SecretVerificationRequest): Promise<void>;
-}
