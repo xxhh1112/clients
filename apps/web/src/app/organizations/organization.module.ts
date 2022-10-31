921cdde87e148728ace8b38d2c232c10a0399525
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-31 16:12:33 +0100

    [EC-73] feat: create new shared org module

--- apps/web/src/app/organizations/organization.module.ts
+++ apps/web/src/app/organizations/organization.module.ts
@@ -5,8 +5,15 @@ import { SharedModule } from "../shared";
 import { AccessSelectorModule } from "./components/access-selector";
 import { CollectionDialogModule } from "./components/collection-dialog";
 import { OrganizationsRoutingModule } from "./organization-routing.module";
+import { SharedOrganizationModule } from "./shared";
 
 @NgModule({
-  imports: [SharedModule, AccessSelectorModule, CollectionDialogModule, OrganizationsRoutingModule],
+  imports: [
+    SharedModule,
+    AccessSelectorModule,
+    CollectionDialogModule,
+    OrganizationsRoutingModule,
+    SharedOrganizationModule,
+  ],
 })
 export class OrganizationModule {}
