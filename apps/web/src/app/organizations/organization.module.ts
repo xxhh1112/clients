1a366ad53c75e470a23d77a668039fadcaf4c322
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-31 16:38:55 +0100

    [EC-73] feat: create core organization module

--- apps/web/src/app/organizations/organization.module.ts
+++ apps/web/src/app/organizations/organization.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from "@angular/core";
 
 import { SharedModule } from "../shared";
 
+import { CoreOrganizationModule } from "./core";
 import { OrganizationsRoutingModule } from "./organization-routing.module";
 import { SharedOrganizationModule } from "./shared";
 
 @NgModule({
-  imports: [SharedModule, OrganizationsRoutingModule, SharedOrganizationModule],
+  imports: [
+    SharedModule,
+    OrganizationsRoutingModule,
+    SharedOrganizationModule,
+    CoreOrganizationModule,
+  ],
 })
 export class OrganizationModule {}
