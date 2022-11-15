da5d0c60aa79a3964e6bdb84f8770fa6ae995346
rr-bw <rrichetto@bitwarden.com>
2022-11-15 13:26:22 -0800

    simplify and strip out password logic

--- libs/components/src/progress/progress.component.ts
+++ libs/components/src/progress/progress.component.ts
@@ -1,128 +1,36 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
-import { PasswordGenerationService } from "@bitwarden/common/abstractions/passwordGeneration.service";
-
-export interface PasswordColorText {
-  color: string;
-  text: string;
-}
+type SizeTypes = "large" | "default" | "small";
+type ProgressTypes = "percent" | "strength";
 
 @Component({
   selector: "bit-progress",
   templateUrl: "./progress.component.html",
 })
-export class ProgressComponent implements OnChanges {
-  @Input() showText = false;
-  @Input() email: string;
-  @Input() name: string;
-  @Input() set password(value: string) {
-    this.updatePasswordStrength(value);
-  }
+export class ProgressComponent {
+  @Input() showText = true;
+  @Input() size: SizeTypes = "default";
+  @Input() type: ProgressTypes = "percent";
 
-  @Output() passwordStrengthResult = new EventEmitter<any>();
-  @Output() passwordScoreColor = new EventEmitter<PasswordColorText>();
+  barWidth: 0;
 
-  masterPasswordScore: number;
-  scoreWidth = 0;
-  color = "bg-danger";
-  text: string;
-
-  private masterPasswordStrengthTimeout: any;
-
-  // used by desktop and browser to display strength text color
-  get masterPasswordScoreColor() {
-    switch (this.masterPasswordScore) {
-      case 4:
-        return "success";
-      case 3:
-        return "primary";
-      case 2:
-        return "warning";
+  get sizeClass() {
+    switch (this.size) {
+      case "small":
+        return `tw-h-1`;
+      case "large":
+        return `tw-h-6`;
       default:
-        return "danger";
+        return `tw-h-4`;
     }
   }
 
-  // used by desktop and browser to display strength text
-  get masterPasswordScoreText() {
-    switch (this.masterPasswordScore) {
-      case 4:
-        return this.i18nService.t("strong");
-      case 3:
-        return this.i18nService.t("good");
-      case 2:
-        return this.i18nService.t("weak");
+  get typeClass() {
+    switch (this.type) {
+      case "strength":
+        return `tw-bg-danger-500`;
       default:
-        return this.masterPasswordScore != null ? this.i18nService.t("weak") : null;
+        return `tw-bg-primary-500`;
     }
-  }
-
-  constructor(
-    private i18nService: I18nService,
-    private passwordGenerationService: PasswordGenerationService
-  ) {}
-
-  ngOnChanges(): void {
-    this.masterPasswordStrengthTimeout = setTimeout(() => {
-      this.scoreWidth = this.masterPasswordScore == null ? 0 : (this.masterPasswordScore + 1) * 20;
-      switch (this.masterPasswordScore) {
-        case 4:
-          this.color = "tw-bg-success-500";
-          this.text = this.i18nService.t("strong");
-          break;
-        case 3:
-          this.color = "tw-bg-primary-500";
-          this.text = this.i18nService.t("good");
-          break;
-        case 2:
-          this.color = "tw-bg-warning-500";
-          this.text = this.i18nService.t("weak");
-          break;
-        default:
-          this.color = "tw-bg-danger-500";
-          this.text = this.masterPasswordScore != null ? this.i18nService.t("weak") : null;
-          break;
-      }
-      this.setPasswordScoreText(this.color, this.text);
-    }, 300);
-  }
-
-  updatePasswordStrength(password: string) {
-    const masterPassword = password;
-    if (this.masterPasswordStrengthTimeout != null) {
-      clearTimeout(this.masterPasswordStrengthTimeout);
-    }
-    const strengthResult = this.passwordGenerationService.passwordStrength(
-      masterPassword,
-      this.getPasswordStrengthUserInput()
-    );
-    this.passwordStrengthResult.emit(strengthResult);
-    this.masterPasswordScore = strengthResult == null ? null : strengthResult.score;
-  }
-
-  getPasswordStrengthUserInput() {
-    let userInput: string[] = [];
-    const email = this.email;
-    const name = this.name;
-    const atPosition = email?.indexOf("@");
-    if (atPosition > -1) {
-      userInput = userInput.concat(
-        email
-          .substr(0, atPosition)
-          .trim()
-          .toLowerCase()
-          .split(/[^A-Za-z0-9]/)
-      );
-    }
-    if (name != null && name !== "") {
-      userInput = userInput.concat(name.trim().toLowerCase().split(" "));
-    }
-    return userInput;
-  }
-
-  setPasswordScoreText(color: string, text: string) {
-    color = color.slice(3);
-    this.passwordScoreColor.emit({ color: color, text: text });
   }
 }
--- libs/components/src/progress/progress.stories.ts
+++ libs/components/src/progress/progress.stories.ts
@@ -1,7 +1,6 @@
 import { Meta, Story, moduleMetadata } from "@storybook/angular";
 
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
-import { PasswordGenerationService } from "@bitwarden/common/abstractions/passwordGeneration.service";
 
 import { I18nMockService } from "../utils/i18n-mock.service";
 
@@ -23,14 +22,6 @@ export default {
             });
           },
         },
-        {
-          provide: PasswordGenerationService,
-          useValue: {
-            passwordStrength: () => {
-              return;
-            },
-          },
-        },
       ],
     }),
   ],
@@ -41,29 +32,11 @@ export default {
       url: "https://www.figma.com/file/Zt3YSeb6E6lebAffrNLa0h/Tailwind-Component-Library?node-id=1717%3A15868",
     },
   },
-  args: {
-    showText: true,
-  },
 } as Meta;
 
 const Template: Story<ProgressComponent> = (args: ProgressComponent) => ({
   props: args,
 });
 
-export const Empty = Template.bind({});
-Empty.args = {};
-
-export const Weak = Template.bind({});
-Weak.args = {
-  password: "2j&$P$QC",
-};
-
-export const Good = Template.bind({});
-Good.args = {
-  password: "BF^t6%b4zV",
-};
-
-export const Strong = Template.bind({});
-Strong.args = {
-  password: "$BuA2%p7Mt#avU7J",
-};
+export const Default = Template.bind({});
+Default.args = {};
