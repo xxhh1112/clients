b753a35f7e5ad071de00cf7ced5e8b86e84e4196
Jacob Fink <jfink@bitwarden.com>
2022-11-15 10:38:12 -0500

    [EC-14] add back the default assignment to deleted - we need this default assignment to check for null or undefined values

--- apps/web/src/app/organizations/vault/ciphers.component.ts
+++ apps/web/src/app/organizations/vault/ciphers.component.ts
@@ -124,7 +124,7 @@ export class CiphersComponent extends BaseCiphersComponent implements OnDestroy 
   }
 
   async load(filter: (cipher: CipherView) => boolean = null, deleted = false) {
-    this.deleted = deleted;
+    this.deleted = deleted ?? false;
     await this.applyFilter(filter);
     this.loaded = true;
   }
