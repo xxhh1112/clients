02fe5bf0c5421580a88e81c15008aeca75608fbf
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-11-18 10:20:51 +0100

    [EC-598] feat: succesfully intercept methods

--- apps/browser/src/content/webauthn/page-script.ts
+++ apps/browser/src/content/webauthn/page-script.ts
@@ -1,2 +1,21 @@
 // eslint-disable-next-line no-console
 console.log("page-script loaded");
+
+const browserCredentials = {
+  create: navigator.credentials.create.bind(navigator.credentials),
+  get: navigator.credentials.get.bind(navigator.credentials),
+};
+
+// Intercept
+
+navigator.credentials.create = async (options?: CredentialCreationOptions): Promise<Credential> => {
+  alert("Intercepted: create");
+
+  return await browserCredentials.create(options);
+};
+
+navigator.credentials.get = async (options?: CredentialRequestOptions): Promise<Credential> => {
+  alert("Intercepted: get");
+
+  return await browserCredentials.get(options);
+};
