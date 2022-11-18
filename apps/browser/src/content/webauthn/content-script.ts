62cfcb6dc09420ee086eecc5fdba51a29f832247
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-11-18 10:01:33 +0100

    [EC-598] feat: load page script from content script

--- apps/browser/src/content/webauthn/content-script.ts
+++ apps/browser/src/content/webauthn/content-script.ts
@@ -1,2 +1,6 @@
 // eslint-disable-next-line no-console
 console.log("content-script loaded");
+
+const s = document.createElement("script");
+s.src = chrome.runtime.getURL("content/webauthn/page-script.js");
+(document.head || document.documentElement).appendChild(s);
