7e077da7e229f4d9f2371f40ac77259cc307ac93
Jacob Fink <jfink@bitwarden.com>
2022-11-23 08:49:07 -0500

    [EC-14] remove CollectionGroupDetailsView models

--- libs/common/src/models/data/collection.data.ts
+++ libs/common/src/models/data/collection.data.ts
@@ -1,8 +1,4 @@
-import {
-  CollectionDetailsResponse,
-  CollectionGroupDetailsResponse,
-} from "../response/collection.response";
-import { SelectionReadOnlyResponse } from "../response/selection-read-only.response";
+import { CollectionDetailsResponse } from "../response/collection.response";
 
 export class CollectionData {
   id: string;
@@ -11,23 +7,11 @@ export class CollectionData {
   externalId: string;
   readOnly: boolean;
 
-  constructor(response: CollectionDetailsResponse | CollectionGroupDetailsResponse) {
+  constructor(response: CollectionDetailsResponse) {
     this.id = response.id;
     this.organizationId = response.organizationId;
     this.name = response.name;
     this.externalId = response.externalId;
-
-    if (response instanceof CollectionDetailsResponse) {
-      this.readOnly = response.readOnly;
-    }
-  }
-}
-
-export class CollectionGroupDetailsData extends CollectionData {
-  groups: SelectionReadOnlyResponse[] = [];
-
-  constructor(response: CollectionGroupDetailsResponse) {
-    super(response);
-    this.groups = response.groups;
+    this.readOnly = response.readOnly;
   }
 }
--- libs/common/src/models/view/collection.view.ts
+++ libs/common/src/models/view/collection.view.ts
@@ -1,10 +1,6 @@
 import { Collection } from "../domain/collection";
 import { ITreeNodeObject } from "../domain/tree-node";
-import { SelectionReadOnlyRequest } from "../request/selection-read-only.request";
-import {
-  CollectionResponse,
-  CollectionAccessDetailsResponse,
-} from "../response/collection.response";
+import { CollectionResponse } from "../response/collection.response";
 
 import { View } from "./view";
 
@@ -16,33 +12,17 @@ export class CollectionView implements View, ITreeNodeObject {
   readOnly: boolean = null;
   hidePasswords: boolean = null;
 
-  constructor(c?: Collection | CollectionResponse | Partial<CollectionView>) {
+  constructor(c?: Collection | CollectionResponse) {
     if (!c) {
       return;
     }
+
+    this.id = c.id;
+    this.organizationId = c.organizationId;
+    this.externalId = c.externalId;
     if (c instanceof Collection) {
-      this.id = c.id;
-      this.organizationId = c.organizationId;
-      this.externalId = c.externalId;
       this.readOnly = c.readOnly;
       this.hidePasswords = c.hidePasswords;
-    }
-    Object.assign(this, c);
-  }
-}
-
-export class CollectionGroupDetailsView extends CollectionView {
-  groups: SelectionReadOnlyRequest[] = [];
-
-  constructor(
-    c?: Collection | CollectionAccessDetailsResponse | Partial<CollectionGroupDetailsView>
-  ) {
-    super(c);
-    if (!c) {
-      return;
-    }
-    if (c instanceof CollectionAccessDetailsResponse) {
-      this.groups = c.groups;
     }
   }
 }
