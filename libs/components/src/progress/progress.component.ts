c73ebbc4b38ab7bfa468dfc6ffb9bc5024f721b4
rr-bw <rrichetto@bitwarden.com>
2022-11-26 13:01:52 -0800

    refactor css

--- libs/components/src/progress/progress.component.ts
+++ libs/components/src/progress/progress.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from "@angular/core";
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 
 type SizeTypes = "small" | "default" | "large";
-type ProgressTypes = "progress" | "strength";
+// type ProgressTypes = "progress" | "strength";
 
-const sizeClasses: Record<SizeTypes, string> = {
-  small: "tw-h-1",
-  default: "tw-h-4",
-  large: "tw-h-6",
+const SizeClasses: Record<SizeTypes, string[]> = {
+  small: ["tw-h-1"],
+  default: ["tw-h-4"],
+  large: ["tw-h-6"],
 };
 
 @Component({
@@ -19,45 +19,61 @@ export class ProgressComponent {
   @Input() barWidth = 0;
   @Input() showText = true;
   @Input() size: SizeTypes = "default";
-  @Input() type: ProgressTypes = "progress";
+  // @Input() type: ProgressTypes = "progress";
 
   constructor(private i18nService: I18nService) {}
 
-  // Only show the text if it will fit in the inner bar
+  // Only show the text that fits in the inner bar
   get displayText() {
     return this.showText && this.size !== "small" && this.barWidth > 3;
   }
 
-  get sizeClass() {
-    return sizeClasses[this.size];
+  get outerBarStyles() {
+    return ["tw-overflow-hidden", "tw-rounded", "tw-bg-secondary-100"].concat(
+      SizeClasses[this.size]
+    );
   }
 
-  get bgClass() {
-    if (this.type === "strength") {
-      if (this.barWidth <= 25) {
-        return "tw-bg-danger-500";
-      }
-      if (this.barWidth <= 50) {
-        return "tw-bg-warning-500";
-      }
-      if (this.barWidth <= 99) {
-        return "tw-bg-primary-500";
-      }
-      return "tw-bg-success-500";
-    }
-    return `tw-bg-primary-500`;
+  get innerBarStyles() {
+    return [
+      "tw-flex",
+      "tw-items-center",
+      "tw-justify-center",
+      "tw-whitespace-nowrap",
+      "tw-text-xs",
+      "tw-font-semibold",
+      "tw-text-contrast",
+      "tw-transition-all",
+      "tw-bg-primary-500",
+    ].concat(SizeClasses[this.size]);
+
+    // if (this.type === "strength") {
+    //   if (this.barWidth <= 25) {
+    //     return "tw-bg-danger-500";
+    //   }
+    //   if (this.barWidth <= 50) {
+    //     return "tw-bg-warning-500";
+    //   }
+    //   if (this.barWidth <= 99) {
+    //     return "tw-bg-primary-500";
+    //   }
+    //   return "tw-bg-success-500";
+    // }
+    // return `tw-bg-primary-500`;
   }
 
   get textContent() {
-    if (this.type === "strength") {
-      if (this.barWidth <= 50) {
-        return this.i18nService.t("weak");
-      }
-      if (this.barWidth <= 99) {
-        return this.i18nService.t("good");
-      }
-      return this.i18nService.t("strong");
-    }
-    return `${this.barWidth}%`;
+    return this.barWidth + "%";
+
+    // if (this.type === "strength") {
+    //   if (this.barWidth <= 50) {
+    //     return this.i18nService.t("weak");
+    //   }
+    //   if (this.barWidth <= 99) {
+    //     return this.i18nService.t("good");
+    //   }
+    //   return this.i18nService.t("strong");
+    // }
+    // return `${this.barWidth}%`;
   }
 }
