08e8741a04b6db32ee98f657ee3a2697e560c62f
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2022-11-29 12:45:20 -0600

    [SM-196] Create Access Token Dialog (#4095)
    * Add create access token dialog
    
    * Use ServiceAccountView for access token creation
    
    * Set version to readonly for access token
    
    * DRY up Expiration Date & bug fix
    
    * Break out expiration options component
    
    * Move expiration-options to layout; Match FIGMA
    
    * Create Generic Key generator
    
    * Add getByServiceAccountId
    
    * Change to use keyMaterial and not the full key
    
    * Use access token id, not service account
    
    * Remove generic key generator
    
    * Swap to service account name placeholder
    
    * Swap ExpirationOptions to a CVA
    
    * No longer masking according to FIGMA
    
    * Remove schema comment
    
    * Code review updates
    
    * Update required logic and approach
    
    * Move ExpirationOptionsComponent into access
    
    Co-authored-by: Hinton <hinton@users.noreply.github.com>

--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-tokens.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-tokens.component.ts
@@ -1,33 +1,61 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable, switchMap } from "rxjs";
+import { combineLatestWith, Observable, startWith, switchMap } from "rxjs";
 
-import { ServiceAccountService } from "../service-account.service";
+import { DialogService } from "@bitwarden/components";
+
+import { ServiceAccountView } from "../../models/view/service-account.view";
+import { AccessTokenView } from "../models/view/access-token.view";
+
+import { AccessService } from "./access.service";
+import {
+  AccessTokenOperation,
+  AccessTokenCreateDialogComponent,
+} from "./dialogs/access-token-create-dialog.component";
 
 @Component({
   selector: "sm-access-tokens",
   templateUrl: "./access-tokens.component.html",
 })
 export class AccessTokenComponent implements OnInit {
-  accessTokens$: Observable<any[]>;
+  accessTokens$: Observable<AccessTokenView[]>;
+
+  private serviceAccountId: string;
+  private organizationId: string;
 
   constructor(
     private route: ActivatedRoute,
-    private serviceAccountService: ServiceAccountService
+    private accessService: AccessService,
+    private dialogService: DialogService
   ) {}
 
   ngOnInit() {
-    this.accessTokens$ = this.route.params.pipe(
-      switchMap(async (params) => {
-        return await this.serviceAccountService.getAccessTokens(
-          params.organizationId,
-          params.serviceAccountId
-        );
+    this.accessTokens$ = this.accessService.accessToken$.pipe(
+      startWith(null),
+      combineLatestWith(this.route.params),
+      switchMap(async ([_, params]) => {
+        this.organizationId = params.organizationId;
+        this.serviceAccountId = params.serviceAccountId;
+        return await this.getAccessTokens();
       })
     );
   }
 
-  openNewAccessTokenDialog() {
-    // TODO
+  private async getAccessTokens(): Promise<AccessTokenView[]> {
+    return await this.accessService.getAccessTokens(this.organizationId, this.serviceAccountId);
+  }
+
+  async openNewAccessTokenDialog() {
+    // TODO once service account names are implemented in service account contents page pass in here.
+    const serviceAccountView = new ServiceAccountView();
+    serviceAccountView.id = this.serviceAccountId;
+    serviceAccountView.name = "placeholder";
+
+    this.dialogService.open<unknown, AccessTokenOperation>(AccessTokenCreateDialogComponent, {
+      data: {
+        organizationId: this.organizationId,
+        serviceAccountView: serviceAccountView,
+      },
+    });
   }
 }
