02e9621fa25ea8dd72c83f65c06228762638b70e
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-12-05 13:31:40 +0100

    [EC-598] feat: add b64 conversion

--- libs/common/src/abstractions/fido2/fido2-utils.ts
+++ libs/common/src/abstractions/fido2/fido2-utils.ts
@@ -1,9 +1,21 @@
+import { Utils } from "../../misc/utils";
+
 export class Fido2Utils {
-  static bufferToString(buffer: BufferSource): string {
-    return "";
+  static bufferToString(bufferSource: BufferSource): string {
+    const buffer = Fido2Utils.bufferSourceToUint8Array(bufferSource);
+
+    return Utils.fromBufferToUrlB64(buffer);
   }
 
   static stringToBuffer(str: string): Uint8Array {
-    return new Uint8Array(0);
+    return Utils.fromUrlB64ToArray(str);
+  }
+
+  private static bufferSourceToUint8Array(bufferSource: BufferSource) {
+    if (bufferSource instanceof ArrayBuffer) {
+      return new Uint8Array(bufferSource);
+    } else {
+      return new Uint8Array(bufferSource.buffer);
+    }
   }
 }
