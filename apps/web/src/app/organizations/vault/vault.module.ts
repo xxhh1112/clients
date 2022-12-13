5b4e8ff1cd3bd4798633d3a3b5b5adf490d623db
Shane Melton <smelton@bitwarden.com>
2022-12-13 08:24:44 -0800

    Fix strictTemplating warnings/error after merge with master

--- apps/web/src/app/organizations/vault/vault.module.ts
+++ apps/web/src/app/organizations/vault/vault.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { LooseComponentsModule } from "../../shared/loose-components.module";
 import { SharedModule } from "../../shared/shared.module";
 import { OrganizationBadgeModule } from "../../vault/organization-badge/organization-badge.module";
+import { PipesModule } from "../../vault/pipes/pipes.module";
 
 import { VaultFilterModule } from "./vault-filter/vault-filter.module";
 import { VaultItemsComponent } from "./vault-items.component";
@@ -16,6 +17,7 @@ import { VaultComponent } from "./vault.component";
     SharedModule,
     LooseComponentsModule,
     OrganizationBadgeModule,
+    PipesModule,
   ],
   declarations: [VaultComponent, VaultItemsComponent],
   exports: [VaultComponent],
