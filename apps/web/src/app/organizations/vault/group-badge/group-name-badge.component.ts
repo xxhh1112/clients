ee189113ac57e78576b041efa4f60d2c3f04ba6e
Shane Melton <smelton@bitwarden.com>
2023-01-03 07:13:06 -0800

    [EC-871] Use bit-badge-list component for collection group column (#4341)

--- apps/web/src/app/organizations/vault/group-badge/group-name-badge.component.ts
+++ apps/web/src/app/organizations/vault/group-badge/group-name-badge.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { SelectionReadOnlyRequest } from "@bitwarden/common/models/request/selection-read-only.request";
 
 import { GroupView } from "../../core";
@@ -8,19 +9,19 @@ import { GroupView } from "../../core";
   selector: "app-group-badge",
   templateUrl: "group-name-badge.component.html",
 })
-export class GroupNameBadgeComponent {
+export class GroupNameBadgeComponent implements OnChanges {
   @Input() selectedGroups: SelectionReadOnlyRequest[];
   @Input() allGroups: GroupView[];
 
-  get shownGroups(): SelectionReadOnlyRequest[] {
-    return this.showXMore ? this.selectedGroups.slice(0, 2) : this.selectedGroups;
-  }
+  protected groupNames: string[] = [];
 
-  get showXMore(): boolean {
-    return this.selectedGroups.length > 3;
-  }
+  constructor(private i18nService: I18nService) {}
 
-  get xMoreCount(): number {
-    return this.selectedGroups.length - 2;
+  ngOnChanges() {
+    this.groupNames = this.selectedGroups
+      .map((g) => {
+        return this.allGroups.find((o) => o.id === g.id)?.name;
+      })
+      .sort(this.i18nService.collator.compare);
   }
 }
