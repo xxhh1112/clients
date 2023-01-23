8cd1d15c01f86d74ad396a44357bde0f78689133
Hinton <hinton@users.noreply.github.com>
2023-01-23 09:47:56 +0100

    Refactor

--- apps/desktop/native/crates/core/src/ipc.rs
+++ apps/desktop/native/crates/core/src/ipc.rs
@@ -6,7 +6,7 @@ use anyhow::Result;
 use once_cell::sync::Lazy;
 use parity_tokio_ipc::{Endpoint, SecurityAttributes};
 use tokio::{
-    io::{split, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    io::{split, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
     sync::Mutex,
 };
 
@@ -37,9 +37,21 @@ static INSTANCE: Lazy<Mutex<IpcContext>> = Lazy::new(|| {
     })
 });
 
+struct Connection {
+    client_id: u32,
+    writer: Box<dyn Ty>,
+}
+
+impl Connection {
+    async fn send(&mut self, msg: &str) -> Result<()> {
+        self.writer.write_all(msg.as_bytes()).await.map_err(anyhow::Error::from)
+    }
+}
+
 /// Start the IPC server.
 ///
-/// TODO: Detangle this mess
+/// TODO: Untangle this mess
+/// TODO: We should figure out a way to send messages only to a single client!
 pub async fn start(tx: tokio::sync::mpsc::Sender<Message>) {
     let path = path();
 
@@ -49,7 +61,7 @@ pub async fn start(tx: tokio::sync::mpsc::Sender<Message>) {
     let incoming = endpoint.incoming().expect("failed to open new socket");
     futures::pin_mut!(incoming);
 
-    let connections: Arc<Mutex<Vec<Arc<Mutex<Box<dyn Ty>>>>>> = Arc::new(Mutex::new(Vec::new()));
+    let connections: Arc<Mutex<Vec<Connection>>> = Arc::new(Mutex::new(Vec::new()));
 
     let c = connections.clone();
     tokio::spawn(async move {
@@ -59,7 +71,7 @@ pub async fn start(tx: tokio::sync::mpsc::Sender<Message>) {
                 print!("Sending message2: {}", msg);
                 for connection in c.lock().await.iter_mut() {
                     // Ignore errors, typically caused by a disconnected client.
-                    connection.lock().await.write_all(msg.as_bytes()).await.ok();
+                    connection.send(&msg).await.ok();
                 }
             }
             tokio::time::sleep(std::time::Duration::from_millis(100)).await;
@@ -67,57 +79,66 @@ pub async fn start(tx: tokio::sync::mpsc::Sender<Message>) {
     });
 
     let mut next_client_id = 1;
-    while let Some(result) = incoming.next().await {
-        match result {
-            Ok(stream) => {
-                let (mut reader, writer) = split(stream);
-                let client_id = next_client_id;
-                next_client_id += 1;
+    while let Some(Ok(stream)) = incoming.next().await {
+        let (reader, writer) = split(stream);
 
-                let mutex: Arc<Mutex<Box<dyn Ty>>> = Arc::new(Mutex::new(Box::new(writer)));
+        let client_id = next_client_id;
+        next_client_id += 1;
 
-                connections.lock().await.push(mutex.clone());
-                let tx = tx.clone();
+        let c = Connection {
+            client_id,
+            writer: Box::new(writer),
+        };
 
+        connections.lock().await.push(c);
+
+        let tx = tx.clone();
+
+        tx.send(Message {
+            client_id: client_id,
+            kind: MessageType::Connected,
+            message: "Connected".to_owned(),
+        })
+        .await
+        .unwrap();
+
+        tokio::spawn(async move {
+            read_messages_to_queue(reader, tx, client_id).await;
+        });
+    }
+}
+
+async fn read_messages_to_queue(
+    mut reader: tokio::io::ReadHalf<impl AsyncRead + AsyncWrite>,
+    tx: tokio::sync::mpsc::Sender<Message>,
+    client_id: u32,
+) {
+    loop {
+        // TODO: Is there a better way to read the whole message?
+        let mut buf = vec![0u8; 4096].into_boxed_slice();
+        match reader.read(&mut buf).await {
+            Err(_) | Ok(0) => {
                 tx.send(Message {
                     client_id: client_id,
-                    kind: MessageType::Connected,
-                    message: "Connected".to_owned(),
+                    kind: MessageType::Disconnected,
+                    message: "Disconnected".to_owned(),
                 })
                 .await
                 .unwrap();
-
-                tokio::spawn(async move {
-                    loop {
-                        let mut buf = vec![0u8; 4096].into_boxed_slice();
-                        match reader.read(&mut buf).await {
-                            Err(_) | Ok(0) => {
-                                tx.send(Message {
-                                    client_id: client_id,
-                                    kind: MessageType::Disconnected,
-                                    message: "Disconnected".to_owned(),
-                                })
-                                .await
-                                .unwrap();
-                                break;
-                            }
-                            Ok(n) => {
-                                let msg = std::str::from_utf8(&buf[..n]);
-                                println!("Sending message: {}", msg.unwrap());
-
-                                tx.send(Message {
-                                    client_id: client_id,
-                                    kind: MessageType::Message,
-                                    message: msg.unwrap().to_owned(),
-                                })
-                                .await
-                                .unwrap();
-                            }
-                        }
-                    }
-                });
+                break;
             }
-            _ => unreachable!("ideally"),
+            Ok(n) => {
+                let msg = std::str::from_utf8(&buf[..n]);
+                println!("Sending message: {}", msg.unwrap());
+
+                tx.send(Message {
+                    client_id: client_id,
+                    kind: MessageType::Message,
+                    message: msg.unwrap().to_owned(),
+                })
+                .await
+                .unwrap();
+            }
         }
     }
 }
