5d8fb87a51aa54c1e290795bc23928cac812c0d2
Todd Martin <tmartin@bitwarden.com>
2023-02-05 21:32:43 -0500

    Removed self-hosted check from desktop component.

--- apps/desktop/src/app/accounts/login/login.component.ts
+++ apps/desktop/src/app/accounts/login/login.component.ts
@@ -93,7 +93,7 @@ export class LoginComponent extends BaseLoginComponent implements OnDestroy {
 
   async ngOnInit() {
     await super.ngOnInit();
-    await this.checkSelfHosted();
+    await this.getLoginWithDevice(this.loggedEmail);
     this.broadcasterService.subscribe(BroadcasterSubscriptionId, async (message: any) => {
       this.ngZone.run(() => {
         switch (message.command) {
@@ -140,7 +140,7 @@ export class LoginComponent extends BaseLoginComponent implements OnDestroy {
     // eslint-disable-next-line rxjs-angular/prefer-takeuntil, rxjs/no-async-subscribe
     childComponent.onSaved.subscribe(async () => {
       modal.close();
-      await this.checkSelfHosted();
+      await this.getLoginWithDevice(this.loggedEmail);
     });
   }
 
@@ -176,11 +176,5 @@ export class LoginComponent extends BaseLoginComponent implements OnDestroy {
   private focusInput() {
     const email = this.loggedEmail;
     document.getElementById(email == null || email === "" ? "email" : "masterPassword").focus();
-  }
-
-  private async checkSelfHosted() {
-    this.selfHosted = this.environmentService.isSelfHosted();
-
-    await this.getLoginWithDevice(this.loggedEmail);
   }
 }
