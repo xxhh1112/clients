5a58ec495d5f93e8807ea8624fcce0040a75c947
Jared Snider <jsnider@bitwarden.com>
2023-03-24 17:29:36 -0400

    PM-55 - Create API Helper service which handles http request building, execution, and response handling and replaced internals of send(...) with the helper methods. This should allow me to refactor the TokenApiService to purely use the API helper service with no circular dependency issues.  Still a work in progress as I must update all references to moved methods as required to get the clients building again.

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -2,6 +2,7 @@ import { LOCALE_ID, NgModule } from "@angular/core";
 
 import { AvatarUpdateService as AccountUpdateServiceAbstraction } from "@bitwarden/common/abstractions/account/avatar-update.service";
 import { AnonymousHubService as AnonymousHubServiceAbstraction } from "@bitwarden/common/abstractions/anonymousHub.service";
+import { ApiHelperService as ApiHelperServiceAbstraction } from "@bitwarden/common/abstractions/api-helper.service.abstraction";
 import { ApiService as ApiServiceAbstraction } from "@bitwarden/common/abstractions/api.service";
 import { AppIdService as AppIdServiceAbstraction } from "@bitwarden/common/abstractions/appId.service";
 import { AuditService as AuditServiceAbstraction } from "@bitwarden/common/abstractions/audit.service";
@@ -84,6 +85,7 @@ import { Account } from "@bitwarden/common/models/domain/account";
 import { GlobalState } from "@bitwarden/common/models/domain/global-state";
 import { AvatarUpdateService } from "@bitwarden/common/services/account/avatar-update.service";
 import { AnonymousHubService } from "@bitwarden/common/services/anonymousHub.service";
+import { ApiHelperServiceImplementation } from "@bitwarden/common/services/api-helper.service.implementation";
 import { ApiService } from "@bitwarden/common/services/api.service";
 import { AppIdService } from "@bitwarden/common/services/appId.service";
 import { AuditService } from "@bitwarden/common/services/audit.service";
@@ -363,7 +365,13 @@ import { AbstractThemingService } from "./theming/theming.service.abstraction";
     {
       provide: ApiServiceAbstraction,
       useClass: ApiService,
-      deps: [PlatformUtilsServiceAbstraction, EnvironmentServiceAbstraction, LOGOUT_CALLBACK],
+      deps: [
+        PlatformUtilsServiceAbstraction,
+        EnvironmentServiceAbstraction,
+        ApiHelperServiceAbstraction,
+        TokenApiServiceAbstraction,
+        LOGOUT_CALLBACK,
+      ],
     },
     {
       provide: SendServiceAbstraction,
@@ -651,6 +659,11 @@ import { AbstractThemingService } from "./theming/theming.service.abstraction";
         TokenServiceAbstraction,
         AppIdServiceAbstraction,
       ],
+    },
+    {
+      provide: ApiHelperServiceAbstraction,
+      useClass: ApiHelperServiceImplementation,
+      deps: [PlatformUtilsServiceAbstraction, EnvironmentServiceAbstraction],
     },
   ],
 })
