489c438ff4a5fa75e36a47bdb482890fbc135b1f
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-04-12 10:56:12 +0200

    [EC-598] chore: clean up old commented code

--- libs/common/src/webauthn/abstractions/fido2-user-interface.service.abstraction.ts
+++ libs/common/src/webauthn/abstractions/fido2-user-interface.service.abstraction.ts
@@ -5,22 +5,6 @@ export interface NewCredentialParams {
 
 export abstract class Fido2UserInterfaceService {
   newSession: (abortController?: AbortController) => Promise<Fido2UserInterfaceSession>;
-
-  // confirmCredential: (cipherId: string, abortController?: AbortController) => Promise<boolean>;
-  // pickCredential: (cipherIds: string[], abortController?: AbortController) => Promise<string>;
-  // confirmNewCredential: (
-  //   params: NewCredentialParams,
-  //   abortController?: AbortController
-  // ) => Promise<boolean>;
-  // confirmNewNonDiscoverableCredential: (
-  //   params: NewCredentialParams,
-  //   abortController?: AbortController
-  // ) => Promise<string | undefined>;
-  // informExcludedCredential: (
-  //   existingCipherIds: string[],
-  //   newCredential: NewCredentialParams,
-  //   abortController?: AbortController
-  // ) => Promise<void>;
 }
 
 export abstract class Fido2UserInterfaceSession {
