6f74a8e8894ba0f71f7e31b4149b63c9a41c805c
Hinton <hinton@users.noreply.github.com>
2023-04-20 19:54:05 +0200

    This is awful

--- apps/cli/src/commands/serve.command.ts
+++ apps/cli/src/commands/serve.command.ts
@@ -245,10 +245,8 @@ export class ServeCommand {
     });
 
     router.post("/unlock", async (ctx, next) => {
-      const response = await this.unlockCommand.run(
-        ctx.request.body.password == null ? null : (ctx.request.body.password as string),
-        ctx.request.query
-      );
+      const body = ctx.request.body as { password: string };
+      const response = await this.unlockCommand.run(body?.password, ctx.request.query);
       this.processResponse(ctx.response, response);
       await next();
     });
@@ -285,7 +283,7 @@ export class ServeCommand {
       const response = await this.shareCommand.run(
         ctx.params.id,
         ctx.params.organizationId,
-        ctx.request.body // TODO: Check the format of this body for an array of collection ids
+        ctx.request.body as any // TODO: Check the format of this body for an array of collection ids
       );
       this.processResponse(ctx.response, response);
       await next();
@@ -298,7 +296,7 @@ export class ServeCommand {
       }
       const response = await this.createCommand.run(
         "attachment",
-        ctx.request.body,
+        ctx.request.body as any,
         ctx.request.query,
         {
           fileBuffer: ctx.request.file.buffer,
@@ -330,7 +328,7 @@ export class ServeCommand {
       } else {
         response = await this.createCommand.run(
           ctx.params.object,
-          ctx.request.body,
+          ctx.request.body as any,
           ctx.request.query
         );
       }
@@ -343,10 +341,13 @@ export class ServeCommand {
         await next();
         return;
       }
+
+      const body = ctx.request.body as { id: string };
+
       let response: Response = null;
       if (ctx.params.object === "send") {
-        ctx.request.body.id = ctx.params.id;
-        response = await this.sendEditCommand.run(ctx.request.body, ctx.request.query);
+        body.id = ctx.params.id;
+        response = await this.sendEditCommand.run(body as any, ctx.request.query);
       } else {
         response = await this.editCommand.run(
           ctx.params.object,
