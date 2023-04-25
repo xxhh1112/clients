0dafd851946dd287df5d906db8e35b7ec93d7a01
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-04-25 16:29:35 +0200

    [EC-598] feat: quick fix noop service

--- libs/common/src/fido2/services/noop-fido2-user-interface.service.ts
+++ libs/common/src/fido2/services/noop-fido2-user-interface.service.ts
@@ -1,34 +1,10 @@
-import { RequestAbortedError } from "../abstractions/fido2-client.service.abstraction";
 import {
   Fido2UserInterfaceService as Fido2UserInterfaceServiceAbstraction,
-  NewCredentialParams,
+  Fido2UserInterfaceSession,
 } from "../abstractions/fido2-user-interface.service.abstraction";
 
 export class Fido2UserInterfaceService implements Fido2UserInterfaceServiceAbstraction {
-  async confirmCredential(): Promise<boolean> {
-    return false;
-  }
-
-  pickCredential(): Promise<string> {
-    throw new RequestAbortedError();
-  }
-
-  async confirmNewCredential(): Promise<boolean> {
-    return false;
-  }
-
-  async confirmNewNonDiscoverableCredential(
-    params: NewCredentialParams,
-    abortController?: AbortController
-  ): Promise<string> {
-    return null;
-  }
-
-  async informExcludedCredential(
-    existingCipherIds: string[],
-    newCredential: NewCredentialParams,
-    abortController?: AbortController
-  ): Promise<void> {
-    // Not Implemented
+  newSession(abortController?: AbortController): Promise<Fido2UserInterfaceSession> {
+    throw new Error("Not implemented exception");
   }
 }
