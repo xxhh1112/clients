2c6ccc84d8abe83df1649d03b2caae68abaf8345
André Bispo <abispo@bitwarden.com>
2023-04-26 15:09:52 +0100

    [PM-169] Web: add drop down to select environment

--- apps/web/src/app/layouts/frontend-layout.component.ts
+++ apps/web/src/app/layouts/frontend-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
+import { Utils } from "@bitwarden/common/misc/utils";
 
 @Component({
   selector: "app-frontend-layout",
@@ -9,12 +10,15 @@ import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUti
 export class FrontendLayoutComponent implements OnInit, OnDestroy {
   version: string;
   year = "2015";
+  isEuServer = true;
 
   constructor(private platformUtilsService: PlatformUtilsService) {}
 
   async ngOnInit() {
     this.year = new Date().getFullYear().toString();
     this.version = await this.platformUtilsService.getApplicationVersion();
+    this.isEuServer = Utils.getDomain("https://vault.bitwarden.eu/#/login").includes(".eu");
+    
     document.body.classList.add("layout_frontend");
   }
 
