a371e769d04799389bdba09f3b2b49abc4a73d79
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-05-04 15:55:58 -0500

    [PM-2100] Finishing out tests for the getFormsWithPasswordFields method

--- apps/browser/src/autofill/services/autofill.service.spec.ts
+++ apps/browser/src/autofill/services/autofill.service.spec.ts
@@ -11,6 +11,35 @@ import AutofillPageDetails from "../models/autofill-page-details";
 
 import AutofillService from "./autofill.service";
 
+function createInputFieldDataItem(customFields = {}) {
+  return {
+    opid: "default-input-field-opid",
+    elementNumber: 0,
+    visible: true,
+    viewable: true,
+    htmlID: "default-htmlID",
+    htmlName: "default-htmlName",
+    htmlClass: "default-htmlClass",
+    "label-left": "default-label-left",
+    "label-right": "default-label-right",
+    "label-top": "default-label-top",
+    "label-tag": "default-label-tag",
+    "label-aria": "default-label-aria",
+    placeholder: "default-placeholder",
+    type: "text",
+    value: "default-value",
+    disabled: false,
+    readonly: false,
+    onePasswordFieldType: "",
+    form: "invalidFormId",
+    autoCompleteType: "off",
+    selectInfo: "",
+    maxLength: 0,
+    tagName: "input",
+    ...customFields,
+  };
+}
+
 describe("AutofillService", function () {
   let autofillService: AutofillService;
 
@@ -73,7 +102,6 @@ describe("AutofillService", function () {
     });
 
     it("should return an FormData array containing a form with it's autofill data", function () {
-      jest.spyOn(AutofillService, "loadPasswordFields");
       const usernameInputField = createInputFieldDataItem({
         opid: "username-field",
         form: "validFormId",
@@ -98,34 +126,52 @@ describe("AutofillService", function () {
         },
       ]);
     });
+
+    it("should narrow down three passwords that are present on a page to a single password field to autofill when only one form element is present on the page", function () {
+      const usernameInputField = createInputFieldDataItem({
+        opid: "username-field",
+        form: "validFormId",
+        elementNumber: 1,
+      });
+      const passwordInputField = createInputFieldDataItem({
+        opid: "password-field",
+        type: "password",
+        form: "validFormId",
+        elementNumber: 2,
+      });
+      const secondPasswordInputField = createInputFieldDataItem({
+        opid: "another-password-field",
+        type: "password",
+        form: undefined,
+        elementNumber: 3,
+      });
+      const thirdPasswordInputField = createInputFieldDataItem({
+        opid: "a-third-password-field",
+        type: "password",
+        form: undefined,
+        elementNumber: 4,
+      });
+      pageDetailsMock.fields = [
+        usernameInputField,
+        passwordInputField,
+        secondPasswordInputField,
+        thirdPasswordInputField,
+      ];
+
+      const formData = autofillService.getFormsWithPasswordFields(pageDetailsMock);
+
+      expect(formData).toStrictEqual([
+        {
+          form: pageDetailsMock.forms.validFormId,
+          password: pageDetailsMock.fields[1],
+          passwords: [
+            pageDetailsMock.fields[1],
+            { ...pageDetailsMock.fields[2], form: pageDetailsMock.fields[1].form },
+            { ...pageDetailsMock.fields[3], form: pageDetailsMock.fields[1].form },
+          ],
+          username: pageDetailsMock.fields[0],
+        },
+      ]);
+    });
   });
 });
-
-function createInputFieldDataItem(customFields = {}) {
-  return {
-    opid: "default-input-field-opid",
-    elementNumber: 0,
-    visible: true,
-    viewable: true,
-    htmlID: "default-htmlID",
-    htmlName: "default-htmlName",
-    htmlClass: "default-htmlClass",
-    "label-left": "default-label-left",
-    "label-right": "default-label-right",
-    "label-top": "default-label-top",
-    "label-tag": "default-label-tag",
-    "label-aria": "default-label-aria",
-    placeholder: "default-placeholder",
-    type: "text",
-    value: "default-value",
-    disabled: false,
-    readonly: false,
-    onePasswordFieldType: "",
-    form: "invalidFormId",
-    autoCompleteType: "off",
-    selectInfo: "",
-    maxLength: 0,
-    tagName: "input",
-    ...customFields,
-  };
-}
