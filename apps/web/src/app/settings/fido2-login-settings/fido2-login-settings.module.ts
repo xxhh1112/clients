bc6cff8a66f8907b88687814834193d0d57c6f94
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-05-04 09:18:52 +0200

    [PM-1024] feat: add content to login settings component

--- apps/web/src/app/settings/fido2-login-settings/fido2-login-settings.module.ts
+++ apps/web/src/app/settings/fido2-login-settings/fido2-login-settings.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 
+import { SharedModule } from "../../shared";
+
 import { Fido2LoginSettingsComponent } from "./fido2-login-settings.component";
 
 @NgModule({
-  imports: [],
+  imports: [SharedModule],
   declarations: [Fido2LoginSettingsComponent],
   exports: [Fido2LoginSettingsComponent],
 })
