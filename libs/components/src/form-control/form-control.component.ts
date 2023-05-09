89c5356821a3516bfcdbffbae4708684f9a8a73c
Hinton <hinton@users.noreply.github.com>
2023-05-09 13:02:31 +0200

    Fix label ssue

--- libs/components/src/form-control/form-control.component.ts
+++ libs/components/src/form-control/form-control.component.ts
@@ -45,7 +45,7 @@ export class FormControlComponent {
   }
 
   protected get labelContentClasses() {
-    return ["tw-font-semibold"].concat(
+    return ["tw-inline-flex", "tw-flex-col", "tw-font-semibold"].concat(
       this.formControl.disabled ? "tw-text-muted" : "tw-text-main"
     );
   }
