59a8dee993ff3a1c76f95db4b51288cdf7d07330
Jared Snider <jsnider@bitwarden.com>
2023-05-10 15:56:07 -0400

    PM-1049 - Update DeviceResponse to match latest properties on backend

--- libs/common/src/auth/models/response/device.response.ts
+++ libs/common/src/auth/models/response/device.response.ts
@@ -3,17 +3,21 @@ import { BaseResponse } from "../../../models/response/base.response";
 
 export class DeviceResponse extends BaseResponse {
   id: string;
+  userId: string;
   name: number;
   identifier: string;
   type: DeviceType;
   creationDate: string;
+  revisionDate: string;
 
   constructor(response: any) {
     super(response);
     this.id = this.getResponseProperty("Id");
+    this.userId = this.getResponseProperty("UserId");
     this.name = this.getResponseProperty("Name");
     this.identifier = this.getResponseProperty("Identifier");
     this.type = this.getResponseProperty("Type");
     this.creationDate = this.getResponseProperty("CreationDate");
+    this.revisionDate = this.getResponseProperty("RevisionDate");
   }
 }
