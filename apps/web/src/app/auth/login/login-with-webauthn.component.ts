1e00c82cb39fecf13e26e74c92120b28c5e2e107
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-05-18 11:20:59 +0200

    [PM-2023] feat: full mocked UI implementation

--- apps/web/src/app/auth/login/login-with-webauthn.component.ts
+++ apps/web/src/app/auth/login/login-with-webauthn.component.ts
@@ -1,7 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { WebauthnService } from "../core";
+import { CreatePasskeyFailedIcon } from "../shared/icons/create-passkey-failed.icon";
+import { CreatePasskeyIcon } from "../shared/icons/create-passkey.icon";
+
+type Step = "assert" | "assertFailed";
 
 @Component({
   selector: "app-login-with-webauthn",
   templateUrl: "login-with-webauthn.component.html",
 })
-export class LoginWithWebauthnComponent {}
+export class LoginWithWebauthnComponent implements OnInit {
+  protected readonly Icons = { CreatePasskeyIcon, CreatePasskeyFailedIcon };
+
+  protected currentStep: Step = "assert";
+
+  constructor(private webauthnService: WebauthnService) {}
+
+  ngOnInit(): void {
+    this.authenticate();
+  }
+
+  protected retry() {
+    this.currentStep = "assert";
+    this.authenticate();
+  }
+
+  private async authenticate() {
+    try {
+      return await new Promise((_, reject) => setTimeout(reject, 5000));
+    } catch {
+      this.currentStep = "assertFailed";
+    }
+  }
+}
