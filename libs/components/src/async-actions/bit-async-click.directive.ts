64ce9b8a71e1225eb26011903fb919b3f212ed71
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-06-30 14:41:48 +0200

    [CL-94] feat: remove non-button compatibility

--- libs/components/src/async-actions/bit-async-click.directive.ts
+++ libs/components/src/async-actions/bit-async-click.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener, Input, OnInit, Optional } from "@angular/core";
+import { Directive, HostListener, Input, OnInit } from "@angular/core";
 import { combineLatest } from "rxjs";
 
 import { ButtonLikeAbstraction } from "../shared/button-like.abstraction";
@@ -10,13 +10,11 @@ import { AsyncContextService } from "./async-context.service";
   selector: "[bitAsyncClick]",
 })
 export class BitAsyncClickDirective implements OnInit {
-  private _disabled = false;
-
   @Input("bitAsyncClick") protected handler: FunctionReturningAwaitable;
 
   constructor(
     private asyncContext: AsyncContextService,
-    @Optional() private buttonComponent: ButtonLikeAbstraction
+    private buttonComponent: ButtonLikeAbstraction
   ) {}
 
   ngOnInit(): void {
@@ -24,21 +22,12 @@ export class BitAsyncClickDirective implements OnInit {
       loading: this.asyncContext.loading$,
       currentAction: this.asyncContext.currentAction$,
     }).subscribe(({ loading, currentAction }) => {
-      if (this.buttonComponent == undefined) {
-        this._disabled = loading;
-        return;
-      }
-
       if (currentAction === this.handler) {
         this.buttonComponent.loading = loading;
       } else {
         this.buttonComponent.disabled = loading;
       }
     });
-  }
-
-  @HostBinding("attr.disabled") get disabled() {
-    return this._disabled ? true : null;
   }
 
   @HostListener("click")
