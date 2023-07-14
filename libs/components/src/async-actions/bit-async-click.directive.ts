2573f761a600db5ccbfbedd4fbfd217e01d51d72
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-07-14 15:05:40 +0200

    [CL-94] feat: add ability to stop click propagation

--- libs/components/src/async-actions/bit-async-click.directive.ts
+++ libs/components/src/async-actions/bit-async-click.directive.ts
@@ -1,4 +1,14 @@
-import { Directive, EventEmitter, HostListener, Input, OnInit, Output } from "@angular/core";
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Inject,
+  Input,
+  OnInit,
+  Optional,
+  Output,
+  Provider,
+} from "@angular/core";
 import { combineLatest } from "rxjs";
 
 import { ButtonLikeAbstraction } from "../shared/button-like.abstraction";
@@ -6,6 +16,14 @@ import { FunctionReturningAwaitable } from "../utils/function-to-observable";
 
 import { AsyncContextService } from "./async-context.service";
 import { BitAsyncTag, BitAsyncTaggedEvent } from "./bit-async-tag";
+
+const BIT_ASYNC_CLICK_CONFIG = "BIT_ASYNC_CLICK_CONFIG";
+export interface BitAsyncClickConfig {
+  stopPropagation?: boolean;
+}
+export function BitAsyncClickConfig(config: BitAsyncClickConfig): Provider {
+  return { provide: BIT_ASYNC_CLICK_CONFIG, useValue: config };
+}
 
 @Directive({
   selector: "[bitAsyncClick], (bitAsyncClick)",
@@ -18,7 +36,8 @@ export class BitAsyncClickDirective implements OnInit {
 
   constructor(
     private asyncContext: AsyncContextService,
-    private buttonComponent: ButtonLikeAbstraction
+    private buttonComponent: ButtonLikeAbstraction,
+    @Optional() @Inject(BIT_ASYNC_CLICK_CONFIG) private config?: BitAsyncClickConfig
   ) {}
 
   ngOnInit(): void {
@@ -37,6 +56,10 @@ export class BitAsyncClickDirective implements OnInit {
 
   @HostListener("click", ["$event"])
   protected async onClick($event: MouseEvent) {
+    if (this.config?.stopPropagation) {
+      $event.stopPropagation();
+    }
+
     if (this.buttonComponent.disabled) {
       return;
     }
