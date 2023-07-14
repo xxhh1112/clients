35cd1165b9efbfd1c45c88aa54aa112a07e3bc35
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-07-14 15:03:50 +0200

    [CL-94] feat: implement `bitAsyncDisable` directive

--- libs/components/src/async-actions/async-actions.module.ts
+++ libs/components/src/async-actions/async-actions.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared";
 
 import { BitActionDirective } from "./bit-action.directive";
+import { BitAsyncClickDirective } from "./bit-async-click.directive";
+import { BitAsyncDisableDirective } from "./bit-async-disable.directive";
 import { BitSubmitDirective } from "./bit-submit.directive";
 import { BitFormButtonDirective } from "./form-button.directive";
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [BitActionDirective, BitFormButtonDirective, BitSubmitDirective],
-  exports: [BitActionDirective, BitFormButtonDirective, BitSubmitDirective],
+  declarations: [
+    BitActionDirective,
+    BitFormButtonDirective,
+    BitSubmitDirective,
+    BitAsyncClickDirective,
+    BitAsyncDisableDirective,
+  ],
+  exports: [
+    BitActionDirective,
+    BitFormButtonDirective,
+    BitSubmitDirective,
+    BitAsyncClickDirective,
+    BitAsyncDisableDirective,
+  ],
 })
 export class AsyncActionsModule {}
--- libs/components/src/async-actions/bit-async-disable.directive.ts
+++ libs/components/src/async-actions/bit-async-disable.directive.ts
@@ -1,8 +1,30 @@
-import { Directive } from "@angular/core";
+import { Directive, OnDestroy, OnInit } from "@angular/core";
+import { Subject, takeUntil } from "rxjs";
 
+import { ButtonLikeAbstraction } from "../shared/button-like.abstraction";
+
+import { AsyncContextService } from "./async-context.service";
+
+// TODO: Add MDX docs for this directive
 @Directive({
   selector: "[bitAsyncDisable]",
 })
-export class BitAsyncDisableDirective {
-  // constructor() {}
+export class BitAsyncDisableDirective implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private asyncContext: AsyncContextService,
+    private buttonComponent: ButtonLikeAbstraction
+  ) {}
+
+  ngOnInit(): void {
+    this.asyncContext.loading$.pipe(takeUntil(this.destroy$)).subscribe((loading) => {
+      this.buttonComponent.disabled = loading;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
