b0f6570736f5622eb1f0f1c92478c08420a124b4
rr-bw <102181210+rr-bw@users.noreply.github.com>
2023-07-26 08:34:24 -0700

    add multiple positions for the popover

--- libs/components/src/popover/popover.component.ts
+++ libs/components/src/popover/popover.component.ts
@@ -1,3 +1,4 @@
+import { ConnectedPosition } from "@angular/cdk/overlay";
 import { Component } from "@angular/core";
 
 @Component({
@@ -6,4 +7,38 @@ import { Component } from "@angular/core";
 })
 export class PopoverComponent {
   isOpen = false;
+  overlayPositions: ConnectedPosition[] = [
+    // Popover opens to right of trigger
+    {
+      offsetX: 5,
+      originX: "end",
+      originY: "center",
+      overlayX: "start",
+      overlayY: "center",
+    },
+    // ... to left of trigger
+    {
+      offsetX: -5,
+      originX: "start",
+      originY: "center",
+      overlayX: "end",
+      overlayY: "center",
+    },
+    // ... above trigger
+    {
+      offsetY: -5,
+      originX: "center",
+      originY: "top",
+      overlayX: "center",
+      overlayY: "bottom",
+    },
+    // ... below trigger
+    {
+      offsetY: 5,
+      originX: "center",
+      originY: "bottom",
+      overlayX: "center",
+      overlayY: "top",
+    },
+  ];
 }
