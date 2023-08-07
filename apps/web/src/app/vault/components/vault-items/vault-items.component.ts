581087731c85ae34eea03fd2b1ee9ef1779f6699
Shane Melton <smelton@bitwarden.com>
2023-08-07 11:02:06 -0700

    [AC-1174] Add bulk edit collection access menu option

--- apps/web/src/app/vault/components/vault-items/vault-items.component.ts
+++ apps/web/src/app/vault/components/vault-items/vault-items.component.ts
@@ -177,4 +177,13 @@ export class VaultItemsComponent {
     );
     this.dataSource.data = items;
   }
+
+  protected bulkEditAccess() {
+    this.event({
+      type: "bulkEditAccess",
+      items: this.selection.selected
+        .filter((item) => item.collection !== undefined)
+        .map((item) => item.collection),
+    });
+  }
 }
