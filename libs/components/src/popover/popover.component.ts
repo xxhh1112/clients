61f2622b029ee9abed997f8ddd9ae8e38dec214b
rr-bw <102181210+rr-bw@users.noreply.github.com>
2023-08-16 09:37:35 -0700

    add a11y import

--- libs/components/src/popover/popover.component.ts
+++ libs/components/src/popover/popover.component.ts
@@ -1,8 +1,10 @@
+import { A11yModule } from "@angular/cdk/a11y";
 import { Component, EventEmitter, Input, Output, TemplateRef, ViewChild } from "@angular/core";
 
 @Component({
   standalone: true,
   selector: "bit-popover",
+  imports: [A11yModule],
   templateUrl: "./popover.component.html",
   exportAs: "popoverComponent",
 })
