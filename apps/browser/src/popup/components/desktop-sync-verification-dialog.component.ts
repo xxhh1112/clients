5c99e278b690fb2753f90e57e43da1b770b2182d
William Martin <contact@willmartian.com>
2023-09-01 11:16:09 -0400

    apply code review

--- apps/browser/src/popup/components/desktop-sync-verification-dialog.component.ts
+++ apps/browser/src/popup/components/desktop-sync-verification-dialog.component.ts
@@ -2,12 +2,7 @@ import { DIALOG_DATA } from "@angular/cdk/dialog";
 import { Component, Inject } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import {
-  AsyncActionsModule,
-  ButtonModule,
-  DialogModule,
-  DialogService,
-} from "@bitwarden/components";
+import { ButtonModule, DialogModule, DialogService } from "@bitwarden/components";
 
 export type DesktopSyncVerificationDialogParams = {
   fingerprint: string;
@@ -16,7 +11,7 @@ export type DesktopSyncVerificationDialogParams = {
 @Component({
   templateUrl: "desktop-sync-verification-dialog.component.html",
   standalone: true,
-  imports: [JslibModule, ButtonModule, DialogModule, AsyncActionsModule],
+  imports: [JslibModule, ButtonModule, DialogModule],
 })
 export class DesktopSyncVerificationDialogComponent {
   constructor(@Inject(DIALOG_DATA) protected params: DesktopSyncVerificationDialogParams) {}
--- apps/browser/src/popup/settings/await-desktop-dialog.component.ts
+++ apps/browser/src/popup/settings/await-desktop-dialog.component.ts
@@ -1,17 +1,12 @@
 import { Component } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import {
-  AsyncActionsModule,
-  ButtonModule,
-  DialogModule,
-  DialogService,
-} from "@bitwarden/components";
+import { ButtonModule, DialogModule, DialogService } from "@bitwarden/components";
 
 @Component({
   templateUrl: "await-desktop-dialog.component.html",
   standalone: true,
-  imports: [JslibModule, ButtonModule, DialogModule, AsyncActionsModule],
+  imports: [JslibModule, ButtonModule, DialogModule],
 })
 export class AwaitDesktopDialogComponent {
   static open(dialogService: DialogService) {
