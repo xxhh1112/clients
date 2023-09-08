563e1cc1660f4056fce97916464615e51d5f03f0
Hinton <hinton@users.noreply.github.com>
2023-09-08 18:25:37 +0200

    Remove card from trial

--- apps/web/src/app/auth/trial-initiation/trial-initiation.module.ts
+++ apps/web/src/app/auth/trial-initiation/trial-initiation.module.ts
@@ -9,7 +9,6 @@ import { RegisterFormModule } from "../../auth/register-form/register-form.modul
 import { BillingComponent } from "../../billing/accounts/trial-initiation/billing.component";
 import { EnvironmentSelectorModule } from "../../components/environment-selector/environment-selector.module";
 import { LooseComponentsModule, SharedModule } from "../../shared";
-import { CardComponent } from "../card.component";
 
 import { ConfirmationDetailsComponent } from "./confirmation-details.component";
 import { AbmEnterpriseContentComponent } from "./content/abm-enterprise-content.component";
@@ -40,7 +39,6 @@ import { VerticalStepperModule } from "./vertical-stepper/vertical-stepper.modul
     OrganizationCreateModule,
     LooseComponentsModule,
     EnvironmentSelectorModule,
-    CardComponent,
   ],
   declarations: [
     TrialInitiationComponent,
