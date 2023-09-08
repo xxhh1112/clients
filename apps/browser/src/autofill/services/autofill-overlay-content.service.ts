b2013200c17e659ca6d0b0c53c991f07c237e8ea
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-09-08 14:41:24 -0500

    [PM-2319] Fixing cache invalidation issues present with the collect page details optimization

--- apps/browser/src/autofill/services/autofill-overlay-content.service.ts
+++ apps/browser/src/autofill/services/autofill-overlay-content.service.ts
@@ -56,6 +56,8 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
       return;
     }
 
+    this.removeCachedFormFieldEventListeners(formFieldElement);
+
     formFieldElement.addEventListener(EVENTS.BLUR, this.handleFormFieldBlurEvent);
     formFieldElement.addEventListener(EVENTS.KEYUP, this.handleFormFieldKeyupEvent);
     formFieldElement.addEventListener(
@@ -171,9 +173,31 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
     redirectFocusElement?.focus();
   }
 
+  private removeCachedFormFieldEventListeners(formFieldElement: ElementWithOpId<FormFieldElement>) {
+    const handlers = [EVENTS.INPUT, EVENTS.CLICK, EVENTS.FOCUS];
+    for (let index = 0; index < handlers.length; index++) {
+      const event = handlers[index];
+      const memoIndex = this.getFormFieldHandlerMemoIndex(formFieldElement, event);
+      const existingHandler = this.eventHandlersMemo[memoIndex];
+      if (!existingHandler) {
+        return;
+      }
+
+      formFieldElement.removeEventListener(event, existingHandler);
+      delete this.eventHandlersMemo[memoIndex];
+    }
+  }
+
   private useEventHandlersMemo = (eventHandler: EventListener, memoIndex: string) => {
     return this.eventHandlersMemo[memoIndex] || (this.eventHandlersMemo[memoIndex] = eventHandler);
   };
+
+  private getFormFieldHandlerMemoIndex(
+    formFieldElement: ElementWithOpId<FormFieldElement>,
+    event: string
+  ) {
+    return `${formFieldElement.opid}-${formFieldElement.id}-${event}-handler`;
+  }
 
   private handleFormFieldBlurEvent = () => {
     this.isFieldCurrentlyFocused = false;
@@ -211,7 +235,7 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
   ) => {
     return this.useEventHandlersMemo(
       () => this.triggerFormFieldInput(formFieldElement, autofillFieldData),
-      `${formFieldElement.opid}-${formFieldElement.id}-input-handler`
+      this.getFormFieldHandlerMemoIndex(formFieldElement, EVENTS.INPUT)
     );
   };
 
@@ -256,7 +280,7 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
   private handleFormFieldClickEvent = (formFieldElement: ElementWithOpId<FormFieldElement>) => {
     return this.useEventHandlersMemo(
       () => this.triggerFormFieldClickedAction(formFieldElement),
-      `${formFieldElement.opid}-${formFieldElement.id}-click-handler`
+      this.getFormFieldHandlerMemoIndex(formFieldElement, EVENTS.CLICK)
     );
   };
 
@@ -271,7 +295,7 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
   private handleFormFieldFocusEvent = (formFieldElement: ElementWithOpId<FormFieldElement>) => {
     return this.useEventHandlersMemo(
       () => this.triggerFormFieldFocusedAction(formFieldElement),
-      `${formFieldElement.opid}-${formFieldElement.id}-focus-handler`
+      this.getFormFieldHandlerMemoIndex(formFieldElement, EVENTS.FOCUS)
     );
   };
 
