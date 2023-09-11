96eb57847fd10d2d6fddf719e8e145bfefc10060
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-09-11 17:05:36 -0500

    [PM-2319] Fixing issue present with non visible fields having an overlay element

--- apps/browser/src/autofill/services/autofill-overlay-content.service.ts
+++ apps/browser/src/autofill/services/autofill-overlay-content.service.ts
@@ -477,6 +477,7 @@ class AutofillOverlayContentService implements AutofillOverlayContentServiceInte
     if (
       autofillFieldData.readonly ||
       autofillFieldData.disabled ||
+      !autofillFieldData.viewable ||
       ignoredFieldTypes.has(autofillFieldData.type) ||
       this.keywordsFoundInFieldData(autofillFieldData, ["search", "captcha"])
     ) {
