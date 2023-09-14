2b915a86312e58f7f5c254c28ae035c745e701ff
Thomas Rittson <trittson@bitwarden.com>
2023-09-14 15:03:27 +1000

    Rename routing module to be consistent

--- apps/web/src/app/billing/individual/individual-billing.module.ts
+++ apps/web/src/app/billing/individual/individual-billing.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from "@angular/core";
 import { BillingSharedModule } from "../shared";
 
 import { BillingHistoryViewComponent } from "./billing-history-view.component";
+import { IndividualBillingRoutingModule } from "./individual-billing-routing.module";
 import { PremiumComponent } from "./premium.component";
-import { SubscriptionRoutingModule } from "./subscription-routing.module";
 import { SubscriptionComponent } from "./subscription.component";
 import { UserSubscriptionComponent } from "./user-subscription.component";
 
 @NgModule({
-  imports: [SubscriptionRoutingModule, BillingSharedModule],
+  imports: [IndividualBillingRoutingModule, BillingSharedModule],
   declarations: [
     SubscriptionComponent,
     BillingHistoryViewComponent,
