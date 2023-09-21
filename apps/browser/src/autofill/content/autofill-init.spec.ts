fb3010f0f751c1ee588e7ae222c731386195af7f
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-09-21 16:26:24 -0500

    [PM-3982] Updating unit tests within AutofillInit

--- apps/browser/src/autofill/content/autofill-init.spec.ts
+++ apps/browser/src/autofill/content/autofill-init.spec.ts
@@ -1,16 +1,21 @@
 import { mock } from "jest-mock-extended";
 
+import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
+
 import AutofillPageDetails from "../models/autofill-page-details";
 import AutofillScript from "../models/autofill-script";
+import AutofillOverlayContentService from "../services/autofill-overlay-content.service";
+import { RedirectFocusDirection } from "../utils/autofill-overlay.enum";
 
 import { AutofillExtensionMessage } from "./abstractions/autofill-init";
 import AutofillInit from "./autofill-init";
 
 describe("AutofillInit", () => {
   let autofillInit: AutofillInit;
+  const autofillOverlayContentService = mock<AutofillOverlayContentService>();
 
   beforeEach(() => {
-    autofillInit = new AutofillInit();
+    autofillInit = new AutofillInit(autofillOverlayContentService);
   });
 
   afterEach(() => {
@@ -82,6 +87,250 @@ describe("AutofillInit", () => {
       expect(autofillInit["insertAutofillContentService"].fillForm).toHaveBeenCalledWith(
         fillScript
       );
+    });
+
+    it("will update the isCurrentlyFilling properties of the overlay and focus the recent field after filling", async () => {
+      jest.useFakeTimers();
+      const fillScript = mock<AutofillScript>();
+      jest.spyOn(autofillInit as any, "updateOverlayIsCurrentlyFilling");
+      jest.spyOn(autofillInit["insertAutofillContentService"], "fillForm").mockImplementation();
+      jest
+        .spyOn(autofillInit["autofillOverlayContentService"], "focusMostRecentOverlayField")
+        .mockImplementation();
+
+      await autofillInit["fillForm"](fillScript);
+      jest.advanceTimersByTime(300);
+
+      expect(autofillInit["updateOverlayIsCurrentlyFilling"]).toHaveBeenNthCalledWith(1, true);
+      expect(autofillInit["insertAutofillContentService"].fillForm).toHaveBeenCalledWith(
+        fillScript
+      );
+      expect(autofillInit["updateOverlayIsCurrentlyFilling"]).toHaveBeenNthCalledWith(2, false);
+      expect(
+        autofillInit["autofillOverlayContentService"].focusMostRecentOverlayField
+      ).toHaveBeenCalled();
+    });
+
+    it("will not attempt to focus the most recent field if the autofillOverlayContentService is not present", () => {
+      jest.useFakeTimers();
+      const newAutofillInit = new AutofillInit(undefined);
+      const fillScript = mock<AutofillScript>();
+      jest.spyOn(newAutofillInit as any, "updateOverlayIsCurrentlyFilling");
+      jest.spyOn(newAutofillInit["insertAutofillContentService"], "fillForm").mockImplementation();
+
+      newAutofillInit["fillForm"](fillScript);
+      jest.advanceTimersByTime(300);
+
+      expect(newAutofillInit["updateOverlayIsCurrentlyFilling"]).toHaveBeenNthCalledWith(1, true);
+      expect(newAutofillInit["insertAutofillContentService"].fillForm).toHaveBeenCalledWith(
+        fillScript
+      );
+      expect(newAutofillInit["updateOverlayIsCurrentlyFilling"]).not.toHaveBeenNthCalledWith(
+        2,
+        false
+      );
+    });
+  });
+
+  describe("updateOverlayIsCurrentlyFilling", () => {
+    it("will not update the isCurrentlyFilling value if the autofill overlay content service does not exist", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+
+      newAutofillInit["updateOverlayIsCurrentlyFilling"](true);
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+    });
+
+    it("will update the property that indicates if the overlay is currently filling data", () => {
+      autofillInit["autofillOverlayContentService"].isCurrentlyFilling = false;
+
+      autofillInit["updateOverlayIsCurrentlyFilling"](true);
+
+      expect(autofillInit["autofillOverlayContentService"].isCurrentlyFilling).toBe(true);
+    });
+  });
+
+  describe("openAutofillOverlay", () => {
+    it("will not attempt to open the autofill overlay if the autofillOverlayContentService is not present", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+      const message = {
+        command: "openAutofillOverlay",
+        data: {
+          isFocusingFieldElement: true,
+          isOpeningFullOverlay: true,
+          authStatus: AuthenticationStatus.Unlocked,
+        },
+      };
+
+      newAutofillInit["openAutofillOverlay"](message);
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+    });
+
+    it("will open the autofill overlay", () => {
+      const message = {
+        command: "openAutofillOverlay",
+        data: {
+          isFocusingFieldElement: true,
+          isOpeningFullOverlay: true,
+          authStatus: AuthenticationStatus.Unlocked,
+        },
+      };
+
+      autofillInit["openAutofillOverlay"](message);
+
+      expect(
+        autofillInit["autofillOverlayContentService"].openAutofillOverlay
+      ).toHaveBeenCalledWith(
+        message.data.isFocusingFieldElement,
+        message.data.isOpeningFullOverlay,
+        message.data.authStatus
+      );
+    });
+  });
+
+  describe("blurAndRemoveOverlay", () => {
+    it("will not attempt to blur and remove the overlay if the autofillOverlayContentService is not present", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+      jest.spyOn(newAutofillInit as any, "removeAutofillOverlay");
+
+      newAutofillInit["blurAndRemoveOverlay"]();
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+      expect(newAutofillInit["removeAutofillOverlay"]).not.toHaveBeenCalled();
+    });
+
+    it("will blur the most recently focused feel and remove the autofill overlay", () => {
+      jest.spyOn(autofillInit["autofillOverlayContentService"], "blurMostRecentOverlayField");
+      jest.spyOn(autofillInit as any, "removeAutofillOverlay");
+
+      autofillInit["blurAndRemoveOverlay"]();
+
+      expect(
+        autofillInit["autofillOverlayContentService"].blurMostRecentOverlayField
+      ).toHaveBeenCalled();
+      expect(autofillInit["removeAutofillOverlay"]).toHaveBeenCalled();
+    });
+  });
+
+  describe("removeAutofillOverlay", () => {
+    it("will return early if a field is currently focused", () => {
+      autofillInit["autofillOverlayContentService"].isFieldCurrentlyFocused = true;
+
+      autofillInit["removeAutofillOverlay"]();
+
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlayList
+      ).not.toHaveBeenCalled();
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlay
+      ).not.toHaveBeenCalled();
+    });
+
+    it("will remove the autofill overlay list if the overlay is currently filling", () => {
+      autofillInit["autofillOverlayContentService"].isFieldCurrentlyFocused = false;
+      autofillInit["autofillOverlayContentService"].isCurrentlyFilling = true;
+
+      autofillInit["removeAutofillOverlay"]();
+
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlayList
+      ).toHaveBeenCalled();
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlay
+      ).not.toHaveBeenCalled();
+    });
+
+    it("will remove the entire overlay if the overlay is not currently filling", () => {
+      autofillInit["autofillOverlayContentService"].isFieldCurrentlyFocused = false;
+      autofillInit["autofillOverlayContentService"].isCurrentlyFilling = false;
+
+      autofillInit["removeAutofillOverlay"]();
+
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlayList
+      ).not.toHaveBeenCalled();
+      expect(
+        autofillInit["autofillOverlayContentService"].removeAutofillOverlay
+      ).toHaveBeenCalled();
+    });
+  });
+
+  describe("addNewVaultItemFromOverlay", () => {
+    it("will not add a new vault item if the autofillOverlayContentService is not present", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+
+      newAutofillInit["addNewVaultItemFromOverlay"]();
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+    });
+
+    it("will add a new vault item", () => {
+      autofillInit["addNewVaultItemFromOverlay"]();
+
+      expect(autofillInit["autofillOverlayContentService"].addNewVaultItem).toHaveBeenCalled();
+    });
+  });
+
+  describe("redirectOverlayFocusOut", () => {
+    it("will not attempt to redirect focus if the autofillOverlayContentService does not exist", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+      const message = {
+        command: "redirectOverlayFocusOut",
+        data: {
+          direction: RedirectFocusDirection.Next,
+        },
+      };
+
+      newAutofillInit["redirectOverlayFocusOut"](message);
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+    });
+
+    it("will redirect the overlay focus", () => {
+      const message = {
+        command: "redirectOverlayFocusOut",
+        data: {
+          direction: RedirectFocusDirection.Next,
+        },
+      };
+
+      autofillInit["redirectOverlayFocusOut"](message);
+
+      expect(
+        autofillInit["autofillOverlayContentService"].redirectOverlayFocusOut
+      ).toHaveBeenCalledWith(message.data.direction);
+    });
+  });
+
+  describe("updateIsOverlayCiphersPopulated", () => {
+    it("will not update whether the ciphers are populated if the autofillOverlayContentService does note exist", () => {
+      const newAutofillInit = new AutofillInit(undefined);
+      const message = {
+        command: "updateIsOverlayCiphersPopulated",
+        data: {
+          isOverlayCiphersPopulated: true,
+        },
+      };
+
+      newAutofillInit["updateIsOverlayCiphersPopulated"](message);
+
+      expect(newAutofillInit["autofillOverlayContentService"]).toBe(undefined);
+    });
+
+    it("will update whether the overlay ciphers are populated", () => {
+      const message = {
+        command: "updateIsOverlayCiphersPopulated",
+        data: {
+          isOverlayCiphersPopulated: true,
+        },
+      };
+
+      autofillInit["updateIsOverlayCiphersPopulated"](message);
+
+      expect(
+        autofillInit["autofillOverlayContentService"].setIsOverlayCiphersPopulated
+      ).toHaveBeenCalledWith(message.data.isOverlayCiphersPopulated);
     });
   });
 
--- apps/browser/src/autofill/content/autofill-init.ts
+++ apps/browser/src/autofill/content/autofill-init.ts
@@ -51,7 +51,6 @@ class AutofillInit implements AutofillInitInterface {
    * Initializes the autofill content script, setting up
    * the extension message listeners. This method should
    * be called once when the content script is loaded.
-   * @public
    */
   init() {
     this.setupExtensionMessageListeners();
@@ -63,10 +62,9 @@ class AutofillInit implements AutofillInitInterface {
    * parameter is set to true, the page details will be
    * returned to facilitate sending the details in the
    * response to the extension message.
-   * @param {AutofillExtensionMessage} message
-   * @param {boolean} sendDetailsInResponse
-   * @returns {AutofillPageDetails | void}
-   * @private
+   *
+   * @param message - The extension message.
+   * @param sendDetailsInResponse - Determines whether to send the details in the response.
    */
   private async collectPageDetails(
     message: AutofillExtensionMessage,
@@ -88,8 +86,8 @@ class AutofillInit implements AutofillInitInterface {
 
   /**
    * Fills the form with the given fill script.
-   * @param {AutofillScript} fillScript
-   * @private
+   *
+   * @param fillScript - Script used to fill input elements
    */
   private async fillForm(fillScript: AutofillScript) {
     this.updateOverlayIsCurrentlyFilling(true);
@@ -105,6 +103,11 @@ class AutofillInit implements AutofillInitInterface {
     }, 250);
   }
 
+  /**
+   * Handles updating the overlay is currently filling value.
+   *
+   * @param isCurrentlyFilling - Indicates if the overlay is currently filling
+   */
   private updateOverlayIsCurrentlyFilling(isCurrentlyFilling: boolean) {
     if (!this.autofillOverlayContentService) {
       return;
@@ -113,6 +116,11 @@ class AutofillInit implements AutofillInitInterface {
     this.autofillOverlayContentService.isCurrentlyFilling = isCurrentlyFilling;
   }
 
+  /**
+   * Opens the autofill overlay.
+   *
+   * @param data - The extension message data.
+   */
   private openAutofillOverlay({ data }: AutofillExtensionMessage) {
     if (!this.autofillOverlayContentService) {
       return;
@@ -126,6 +134,11 @@ class AutofillInit implements AutofillInitInterface {
     );
   }
 
+  /**
+   * Blurs the most recent overlay field and removes the overlay. Used
+   * in cases where the background unlock or vault item reprompt popout
+   * is opened.
+   */
   private blurAndRemoveOverlay() {
     if (!this.autofillOverlayContentService) {
       return;
@@ -135,6 +148,11 @@ class AutofillInit implements AutofillInitInterface {
     this.removeAutofillOverlay();
   }
 
+  /**
+   * Removes the autofill overlay if the field is not currently focused.
+   * If the autofill is currently filling, only the overlay list will be
+   * removed.
+   */
   private removeAutofillOverlay() {
     if (
       !this.autofillOverlayContentService ||
@@ -151,6 +169,9 @@ class AutofillInit implements AutofillInitInterface {
     this.autofillOverlayContentService.removeAutofillOverlay();
   }
 
+  /**
+   * Adds a new vault item from the overlay.
+   */
   private addNewVaultItemFromOverlay() {
     if (!this.autofillOverlayContentService) {
       return;
@@ -159,6 +180,11 @@ class AutofillInit implements AutofillInitInterface {
     this.autofillOverlayContentService.addNewVaultItem();
   }
 
+  /**
+   * Redirects the overlay focus out of an overlay iframe.
+   *
+   * @param data - Contains the direction to redirect the focus.
+   */
   private redirectOverlayFocusOut({ data }: AutofillExtensionMessage) {
     if (!this.autofillOverlayContentService) {
       return;
@@ -167,6 +193,11 @@ class AutofillInit implements AutofillInitInterface {
     this.autofillOverlayContentService.redirectOverlayFocusOut(data?.direction);
   }
 
+  /**
+   * Updates whether the current tab has ciphers that can populate the overlay list
+   * @param data - Contains the isOverlayCiphersPopulated value
+   *
+   */
   private updateIsOverlayCiphersPopulated({ data }: AutofillExtensionMessage) {
     if (!this.autofillOverlayContentService) {
       return;
